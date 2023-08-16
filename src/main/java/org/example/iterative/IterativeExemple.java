d5f239349e6246956aa8cca9c09322530874b781
fear1611 <127629681+fear1611@users.noreply.github.com>
2023-08-16 14:01:14 +0300

    Create class IterativeExempleTest

---
+++ src/main/java/org/example/iterative/IterativeExemple.java
@@ -0,0 +1,4 @@
+package org.example.iterative;
+
+public class IterativeExemple {
+}
---
+++ src/test/java/org/example/iterative/IterativeExempleTest.java
@@ -0,0 +1,8 @@
+package org.example.iterative;
+
+import org.junit.jupiter.api.Test;
+
+public class IterativeExempleTest {
+
+
+}
